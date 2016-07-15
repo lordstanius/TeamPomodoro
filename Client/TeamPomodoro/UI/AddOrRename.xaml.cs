c40e2edc7eeaff9b7e479a3116876407c9648699
Mladen Stanisic <lordstanius@gmail.com>
2016-07-15 02:35:15 +0200

    Late save.

---
+++ Client/TeamPomodoro/UI/AddOrRename.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+using TeamPomodoro.Core;
+using TeamPomodoro.Globalization;
+
+namespace TeamPomodoro.UI
+{
+	public partial class AddOrRename : Window
+	{
+		public AddOrRename()
+		{
+			InitializeComponent();
+		}
+
+		void OnMouseDown(object sender, MouseButtonEventArgs e)
+		{
+			Util.WindowHelper.Move(new WindowInteropHelper(this).Handle);
+		}
+
+		void OnSaveClick(object sender, RoutedEventArgs e)
+		{
+			DialogResult = true;
+		}
+	}
+}
+
