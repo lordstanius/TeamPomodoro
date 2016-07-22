97dda79167c5b5dc5146773b68aa85c494a273d1
Mladen Stanisic <lordstanius@gmail.com>
2016-07-22 19:59:22 +0200

    Restructured projects and solution for the main project. Some bugfixes.

---
+++ TeamPomodoro/WebApp/Controllers/TeamsController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Model;
+
+namespace WebApp.Controllers
+{
+	public class TeamsController : ApiController
+	{
+		private PomodoroContext db = new PomodoroContext();
+
+		// GET: api/Teams
+		public IQueryable<Team> GetTeams()
+		{
+			return db.Teams;
+		}
+
+		// GET: api/Teams/5
+		[ResponseType(typeof(Team))]
+		public IHttpActionResult GetTeam(Guid id)
+		{
+			Team team = db.Teams.Find(id);
+			if (team == null)
+				return NotFound();
+
+			return Ok(team);
+		}
+
+		// PUT: api/Teams/5
+		[ResponseType(typeof(void))]
+		public IHttpActionResult PostTeam(Team team)
+		{
+			if (!ModelState.IsValid)
+				return BadRequest(ModelState);
+
+			db.Entry(team).State = EntityState.Modified;
+
+			try
+			{
+				db.SaveChanges();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!TeamExists(team.TeamId))
+					return NotFound();
+
+				throw;
+			}
+
+			return StatusCode(HttpStatusCode.NoContent);
+		}
+
+		// POST: api/Teams
+		[ResponseType(typeof(Team))]
+		public IHttpActionResult PutTeam(Team team)
+		{
+			if (!ModelState.IsValid)
+				return BadRequest(ModelState);
+
+			db.Teams.Add(team);
+
+			try
+			{
+				db.SaveChanges();
+			}
+			catch (DbUpdateException)
+			{
+				if (TeamExists(team.TeamId))
+					return Conflict();
+
+				throw;
+			}
+
+			return CreatedAtRoute("DefaultApi", new { id = team.TeamId }, team);
+		}
+
+		// DELETE: api/Teams/5
+		[ResponseType(typeof(Team))]
+		public IHttpActionResult DeleteTeam(Guid id)
+		{
+			Team team = db.Teams.Find(id);
+			if (team == null)
+				return NotFound();
+
+			db.Teams.Remove(team);
+			db.SaveChanges();
+
+			return Ok(team);
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+				db.Dispose();
+
+			base.Dispose(disposing);
+		}
+
+		private bool TeamExists(Guid id)
+		{
+			return db.Teams.Count(e => e.TeamId == id) > 0;
+		}
+	}
+}
